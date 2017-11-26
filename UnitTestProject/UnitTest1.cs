0f2fd3eb2cb654bf26e5cbb4edd9fba29965cba0
crissttian1990 <34012538+crissttian1990@users.noreply.github.com>
2017-11-26 23:46:17 +0100

    Add files via upload

---
+++ UnitTestProject/UnitTest1.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using LiquidThinkingTest;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace UnitTestProject
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public static void Main(string[] args)
+        {
+            string[] args1 = { "-l", "Maybe the force be with you" };
+            MainClass.Main(args1);
+        }
+
+
+        [TestMethod]
+        public void MethodW()
+        {
+            MainClass.methodW("Maybe the force be with you");
+        }
+
+        [TestMethod]
+        public void MethodL()
+        {
+            MainClass.methodL("Maybe the force be with you");
+        }
+
+        [TestMethod]
+        public void MethodV()
+        {
+            MainClass.methodV("Maybe the force be with you", DateTime.Now);
+        }
+
+        [TestMethod]
+        public void HelpMethod()
+        {
+            MainClass.helpMethod();
+        }
+    }
+}
